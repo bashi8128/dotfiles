e95fbf612148cd4b505a051ab44466c479fa00e1
bashi8128 <itabasi.lm@gmail.com>
2025-06-13 18:16:03 +0900

    chore: Define extra plugins in plugins_extras/lang.lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -11,6 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "plugins_extras" },
     { import = "plugins" },
   },
   defaults = {
